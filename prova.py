6c49e51d650b996fca31d192a262430bca4c5bdf
Giovanni annaloro <99549477+giovannaloro@users.noreply.github.com>
2023-09-24 23:33:19 +0200

    nothing important

---
+++ prova.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import NN 
+#my_nn = NN.NN([2,3,2])
+#my_nn.print_layers()
+#my_nn.print_biases()
+ma = [[[1,2,3],[4,5,6]],[[2,4,5]]]
+print(NN.copy_matrix_array(ma))
+#out = my_nn.compute_forward([1,2])
+#print(out)
+#matrix = [[1,2,3],[3,4,5],[6,7,8],[9,10,11]]
+#NN.print_matrix(matrix)
+#print("")
+#NN.print_matrix(NN.copy_matrix(matrix, True))
+#print("")
+#NN.print_matrix(NN.copy_matrix(matrix,True))
